90c27257768bb4b81aafc4db479814d508bbba7f
Raian Barbosa <raianpbstudio@gmail.com>
2024-11-30 22:45:49 -0300

    add new files

--- server/settings.py
+++ server/settings.py
@@ -115,12 +115,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
 import os
+STATIC_URL = '/static/'
+# Diretório onde os arquivos estáticos do projeto estão localizados (apenas para desenvolvimento)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Diretório para armazenar arquivos estáticos coletados (para produção)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- server/urls.py
+++ server/urls.py
@@ -1,6 +1,11 @@
+from django.conf import settings
 from django.urls import path
 from home import views as home_view
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home_view.download_audio, name='download_audio'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
